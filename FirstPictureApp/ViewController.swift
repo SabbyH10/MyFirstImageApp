b06b66bfacf345c5974c3bf95deeb273554f519d
Sabiha Medina <sabihamedina@Jamess-MacBook-Air.local>
2014-12-05 16:19:29 -0800

    Added new picture

--- FirstPictureApp/ViewController.swift
+++ FirstPictureApp/ViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var PhotoImageView: UIImageView!
+    
+    //Create a place to render the filtered Image
+    let context = CIContext(options: nil)
 
+    @IBAction func ApplyFilter(sender: AnyObject) {
+        //Create an image to filter
+        let inputImage = CIImage(image:PhotoImageView.image)
+        
+        //Create a random color to pass to a filter
+        let randomColor = [kCIInputAngleKey:(Double(arc4random_uniform(314))/100)]
+        //Apply a filter to the image
+        let filteredImage = inputImage.imageByApplyingFilter("CIHueAdjust",withInputParameters: randomColor)
+        
+        //Render the filtered Image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent())
+        
+        //Reflect the change back in the interface
+        PhotoImageView.image = UIImage(CGImage: renderedImage)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
